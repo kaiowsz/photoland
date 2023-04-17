03b69b35b68bb414086ca026af0e6f5368db6eea
Kaio Silveira <kaionpsa@gmail.com>
2023-04-17 19:06:22 -0300

    finished setup

---
+++ client/src/hooks/useFetch.ts
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import { request } from "../request"
+
+const useFetch = (url: string) => {
+    const [data, setData] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        async function fetchData() {
+            setIsLoading(true)
+            try {
+                const res = await request.get(url)
+                setData(res.data.data)
+            } catch (error: any) {
+                setError(error)
+            }
+            setIsLoading(false)
+        }
+        fetchData()
+    }, [url])
+
+    return { data, isLoading, error }
+}
+
+export default useFetch;
