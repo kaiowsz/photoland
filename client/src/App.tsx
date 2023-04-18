27190bd509f12a98d5b6a7c3fccfb9f0acca2388
Kaio Silveira <kaionpsa@gmail.com>
2023-04-18 17:01:48 -0300

    react router config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,36 @@
-function App() {
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import ProductDetails from "./pages/ProductDetails";
+import Search from "./pages/Search";
+
+import { Header, Footer, } from "./components";
+
+function Layout() {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
+
+const router = createBrowserRouter([{
+  element: <Layout />,
+  children: [
+    { path: "/", element: <Home /> },
+    { path: "/products/:id", element: <Products /> },
+    { path: "/product/:id", element: <ProductDetails /> },
+    { path: "/search", element: <Search /> },
+  ]
+}])
+
+function App() {
   return (
     <div className="App">
-
+      <RouterProvider router={router} />
     </div>
   )
 }
