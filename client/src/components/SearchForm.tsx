5270c7cbe7e5c1b70597dda738b67173b03a9d05
Kaio Silveira <kaionpsa@gmail.com>
2023-04-26 17:39:26 -0300

    handling input

--- client/src/components/SearchForm.tsx
+++ client/src/components/SearchForm.tsx
@@ -1,11 +1,45 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { FiSearch } from "react-icons/fi"
+import { useNavigate } from "react-router-dom"
 
 function SearchForm() {
+
+  const navigate = useNavigate()
+  const [searchTerm, setSearchTerm] = useState<string>("")
+  const [isAnimating, setIsAnimating] = useState<boolean>(false)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsAnimating(false);
+    }, 1000)
+
+    return () => clearTimeout(timeout)
+  }, [isAnimating])
+  
+
+  function handleSearchInput(e: React.FormEvent<HTMLInputElement>) {
+    setSearchTerm(e.currentTarget.value)
+  }
+  
+  function handleSubmit(e: any) {
+    e.preventDefault();
+
+    // verify user input
+    if(searchTerm.trim() === "" || searchTerm.length < 1) {
+      setIsAnimating(true)
+      return
+    }
+
+    navigate(`/search?query=${searchTerm}`)
+  }
+
+  
+
   return (
-    <form className="w-full relative">
+    <form onSubmit={handleSubmit} className={`${isAnimating ? "animate-shake" : "animate-none"} w-full relative`}>
       
-      <input 
+      <input
+        onChange={handleSearchInput}
         type="text" 
         className="input" 
         placeholder="Search for a product..." />
